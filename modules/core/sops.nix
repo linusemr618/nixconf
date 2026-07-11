3cecf0b09e717c7e712f7c180a425316285f9ecd
Linus Emmerich <emmerichlinus@gmail.com>
2026-07-11 18:54:37 +0200

    Maybe fix crashes on big updates?

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -3,18 +3,22 @@
   self,
   ...
 }: {
-  flake.modules.nixos.core = {...}: {
+  flake.modules.nixos.core = {config, ...}: {
     imports = [inputs.sops-nix.nixosModules.sops];
     sops = {
       defaultSopsFile = self + "/secrets.yaml";
       age = {
         sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-        #keyFile = "/home/${config.user.name}/.config/sops/age/keys.txt";
+        keyFile = "/home/${config.user.name}/.config/sops/age/keys.txt";
       };
     };
   };
 
-  flake.modules.homeManager.core = {pkgs, ...}: {
+  flake.modules.homeManager.core = {
+    config,
+    pkgs,
+    ...
+  }: {
     imports = [inputs.sops-nix.homeManagerModules.sops];
     home.packages = with pkgs; [
       age
@@ -25,7 +29,7 @@
       defaultSopsFile = self + "/secrets.yaml";
       age = {
         sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-        #keyFile = "/home/${config.user.name}/.config/sops/age/keys.txt";
+        keyFile = "/home/${config.user.name}/.config/sops/age/keys.txt";
       };
     };
   };
